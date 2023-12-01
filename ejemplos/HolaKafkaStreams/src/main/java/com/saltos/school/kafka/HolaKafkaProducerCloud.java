d92aed6fc11a7c42337faf4f7e30502c86cfc867
Carlos Saltos <csaltos@gmail.com>
2023-12-01 03:50:33 -0300

    Init code

---
+++ ejemplos/HolaKafkaStreams/src/main/java/com/saltos/school/kafka/HolaKafkaProducerCloud.java
@@ -0,0 +1,45 @@
+package com.saltos.school.kafka;
+
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.FileInputStream;
+import java.util.Date;
+import java.util.Properties;
+
+public class HolaKafkaProducerCloud {
+    public static void main(String[] args) throws Exception {
+
+        Properties props = new Properties();
+
+        props.load(new FileInputStream("src/main/resources/cluster_0.properties"));
+
+        // this.getClass().getClassLoader().getResourceAsStream("cluster_0.properties");
+        props.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        try (Producer<String, String> producer = new KafkaProducer<>(props)) {
+            for (int i = 0; true; i++) {
+                Thread.sleep(2000);
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>("hola", "key_" + i, "Prueba " + i);
+                producer.send(producerRecord,
+                        (metadata, exception) -> {
+                            if (exception != null) {
+                                System.err.println("Error al insertar el mensaje");
+                                exception.printStackTrace();
+                            } else {
+                                System.out.println("Mensaje enviado");
+                                System.out.println("Partition: " + metadata.partition());
+                                System.out.println("Offset: " + metadata.offset());
+                                System.out.println("Timestamp: " + new Date(metadata.timestamp()));
+                            }
+                        });
+            }
+        }
+
+    }
+}
---
+++ ejemplos/HolaKafkaStreams/src/main/java/com/saltos/school/kafka/HolaKafkaStream.java
@@ -0,0 +1,44 @@
+package com.saltos.school.kafka;
+
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Produced;
+import java.util.Properties;
+import java.io.FileInputStream;
+
+public class HolaKafkaStream {
+
+  public static void main(String[] args) throws Exception {
+
+    Properties props = new Properties();
+
+    props.load(new FileInputStream("src/main/resources/cluster_0.properties"));
+
+    props.setProperty("application.id", "HolaKafkaStream");
+    props.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+    Serde<String> stringSerde = Serdes.String();
+    StreamsBuilder builder = new StreamsBuilder();
+    builder
+        .stream("hola", Consumed.with(stringSerde, stringSerde))
+        .peek((k, v) -> System.out.println("Mensaje nuevo: " + v))
+        .mapValues(s -> s.toUpperCase())
+        .peek((k, v) -> System.out.println("Guardando mensaje: " + v))
+        .to("hola2", Produced.with(stringSerde, stringSerde));
+
+    Topology topology = builder.build();
+
+    KafkaStreams kafkaStreams = new KafkaStreams(topology, props);
+
+    kafkaStreams.start();
+
+    Thread.sleep(120000);
+
+    kafkaStreams.close();
+  }
+}
